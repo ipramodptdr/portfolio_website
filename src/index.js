5cec15ff0fd1860ed2d847eb51c20ec6d63aa049
ipramodptdr <ipramodptdr@gmail.com>
2025-03-13 12:53:11 +0530

    Fixed image imports for production build

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { FaCss3, FaGithub, FaHtml5, FaJs, FaReact } from "react-icons/fa";
 import { FiFramer } from "react-icons/fi";
 import { SiTailwindcss } from "react-icons/si";
+import guessImage from "./assets/Images/guess.png";
+import profileImage from "./assets/images/PNGPROFILE.png";
+
 const admin = {
   firstName: "Pramod ",
   lastName:"Patidar",
   role: "Front-End Developer",
   contactNumber: "+91 99777-03412",
-  image: "/src/assets/images/PNGPROFILE.png",
+  image: profileImage,
   resume: "/resume.pdf",
-  about: "Hi there! I’m Pramod Patidar, a passionate front-end developer with a love for creating visually engaging, interactive, and user-friendly websites. With a solid foundation in HTML, CSS, and JavaScript, I specialize in bringing designs to life with React, Framer Motion, and GSAP to build dynamic and seamless user experience, I am always eager to explore new technologies and stay up-to-date with the latest trends in web development. Whether it’s crafting pixel-perfect layouts, optimizing performance, or implementing smooth animations, I focus on delivering clean, efficient, and aesthetically pleasing code. Let’s create something amazing together!",
+  about: "Hi there! I'm Pramod Patidar, a passionate front-end developer with a love for creating visually engaging, interactive, and user-friendly websites. With a solid foundation in HTML, CSS, and JavaScript, I specialize in bringing designs to life with React, Framer Motion, and GSAP to build dynamic and seamless user experience, I am always eager to explore new technologies and stay up-to-date with the latest trends in web development. Whether it's crafting pixel-perfect layouts, optimizing performance, or implementing smooth animations, I focus on delivering clean, efficient, and aesthetically pleasing code. Let's create something amazing together!",
   skills: [
     {
       names: ["HTML5", "CSS3", "JavaScript", "React", "Framer Motion", "Tailwind CSS", "GitHub"],
@@ -30,7 +33,7 @@ const projects = [
     title: "Guess The Number Game",
     description: "A simple game where the user has to guess the number between 1 and 100.",
     href: "https://lucky-guess.netlify.app//",
-    image: "/src/assets/Images/guess.png"
+    image: guessImage
   },
   {
     id: 2,
